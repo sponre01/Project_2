2444bdd8c5406e8604caaac4c4862639c95d69b4
sponre01 <sponre01@gmail.com>
2019-03-24 10:59:12 -0500

    map with country geojson added

---
+++ Map view/static/js/logic.js
@@ -0,0 +1,50 @@
+// Creating map object
+var map = L.map("map", {
+  center: [30, 0],
+  zoom: 3
+});
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.light",
+  accessToken: API_KEY
+}).addTo(map);
+
+var link = "https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson";
+
+// Grab GEOJSON
+d3.json(link, function(data) {
+  L.geoJson(data, {
+    style: function(feature) {
+      return {
+        color: "white",
+        fillColor: "silver",
+        fillOpacity: 0.5,
+        weight: 1.5
+      };
+    },
+
+  // Mouseover/mouseout events
+    onEachFeature: function(feature, layer) {
+      layer.on({
+        mouseover: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.9
+          });
+        },
+        mouseout: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.5
+          });
+        },
+      });
+      // Popup info
+      layer.bindPopup("<h1>" + feature.properties.ADMIN+ "</h1> <hr> <h2>" + feature.properties.ISO_A3 + "</h2>");
+
+    }
+  }).addTo(map);
+});
