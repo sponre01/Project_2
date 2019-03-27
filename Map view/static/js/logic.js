cecde96b958aa8e45f883fea25c05e8a6ba9b6cd
sponre01 <sponre01@gmail.com>
2019-03-26 21:23:23 -0500

    map outlines and button placeholder done

--- Map view/static/js/logic.js
+++ Map view/static/js/logic.js
@@ -1,3 +1,9 @@
+// silver: #C0C0C0 or #A7A7AD
+// bronze: #cd7f32 or #A77044
+// gold: #ffd700 or #D6AF36
+// grayish violet: #c5c1cf
+
+
 // Creating map object
 var map = L.map("map", {
   center: [30, 0],
@@ -22,7 +28,7 @@ d3.json(link, function(data) {
         color: "white",
         fillColor: "silver",
         fillOpacity: 0.5,
-        weight: 1.5
+        weight: 1.0
       };
     },
 
@@ -43,8 +49,46 @@ d3.json(link, function(data) {
         },
       });
       // Popup info
-      layer.bindPopup("<h1>" + feature.properties.ADMIN + "</h1> <hr> <h2>" + feature.properties.ISO_A3 + "</h2>");
-
+      layer.bindPopup("<h1>" + feature.properties.ADMIN + "</h1> <hr> <h2>" + feature.properties.ISO_A3 + "</h2>" + "<button id='button' type='submit' class='btn btn-default'>See the Breakdown</button>");
     }
   }).addTo(map);
+
+
+// Button
+var button = d3.select("#button");
+  button.on("click", function() {
+    tbody.selectAll('*').remove();
+    var inputDate = d3.select("#datetime");
+    var inputText = inputDate.property("value")
+    var filteredData = tableData.filter(x => x.datetime === inputText);
+      })
+    })
+
+//   // Create a baseMaps object
+// var baseMaps = {
+//   "Street Map": streetmap,
+//   "Dark Map": darkmap
+// };
+
+// // Create an overlay object
+// var overlayMaps = {
+//   "State Population": states,
+//   "City Population": cities
+// };
+
+// // Define a map object
+// var myMap = L.map("map", {
+//   center: [37.09, -95.71],
+//   zoom: 5,
+//   layers: [streetmap, states, cities]
+// });
+
+// // Pass our map layers into our layer control
+// // Add the layer control to the map
+// L.control.layers(baseMaps, overlayMaps, {
+//   collapsed: false
+// }).addTo(myMap);
+
+
+
 });
