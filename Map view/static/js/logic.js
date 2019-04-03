7a90bae6266418f18a50f73481ace86c8e0e9316
sponre01 <sponre01@gmail.com>
2019-04-02 20:39:22 -0500

    map in olympic ring colors

--- Map view/static/js/logic.js
+++ Map view/static/js/logic.js
@@ -1,11 +1,10 @@
 // Create function to color countries based on total medals won
 function getColor(d) {
-    return d > 700 ? '#D6AF36' :
-           d > 400 ? '#FEE101' :
-           d > 200 ? '#D7D7D7' :
-           d > 50  ? '#A7A7AD' :
-           d > 10  ? '#824A02' :
-                     '#A77044';
+    return d > 1000 ? '#3e76ec' :
+           d > 500 ? '#555555' :
+           d > 100 ? '#ff0000' :
+           d > 10  ? '#ffce01' :
+                     '#179a13';
   }
 
 // Creating map object
@@ -49,12 +48,12 @@ d3.json(link, function(data) {
         mouseout: function(event) {
           layer = event.target;
           layer.setStyle({
-            fillOpacity: 0.5
+            fillOpacity: 0.8
           });
         },
       });
       // Popup info
-      layer.bindPopup("<h1>" + feature.properties.ADMIN +"<br>"+ feature.properties.total + " total medals" + "</h1> <hr> <h2>" + 
+      layer.bindPopup("<h1>" + feature.properties.ADMIN +"<br>"+  feature.properties.ISO_A3+"<br>"+feature.properties.total + " total medals" + "</h1> <hr> <h2>" + 
       //feature.properties.ISO_A3 + "</h2>" + 
       "<p><h3> Gold medals: " + feature.properties.gold + "</h3></p>" +
       "<p><h3> Silver medals: " + feature.properties.silver +"</h3></p>" +
@@ -63,4 +62,32 @@ d3.json(link, function(data) {
 
     }
   }).addTo(map);
+
+
+// make legend
+var legend = L.control({position: 'bottomleft'});
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 100, 500, 1000],
+        labels = [];
+        div.innerHTML+='<div><b>Legend</b></div><br>';
+    // loop through our magnitude intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style=\"background:' + getColor(grades[i] + 1) + '\"></li> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+ 
+    return div;
+};
+
+legend.addTo(map);
+
+
+
+
+
+
+
 });
